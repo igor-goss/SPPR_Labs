c2f9df2097c5e74f672c6a0f5d162af2b4b324f0
Igor <igorshevelenkov@gmail.com>
2023-09-07 12:33:08 +0300

    Lab2 dev

---
+++ Web_3_Shevelenkov/Controllers/Product.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_3_Shevelenkov.Controllers
+{
+    public class Product : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
