343a9846fbf1ac2b7868126b2e4026b09af46489
Igor <igorshevelenkov@gmail.com>
2023-10-02 18:10:27 +0300

    Lab4 in process

---
+++ Web_3_Shevelenkov.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_3_Shevelenkov.Domain.Entities;
+
+namespace Web_3_Shevelenkov.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Tank> Tanks { get; set; }
+        public DbSet<TankType> TankTypes { get; set; }
+    }
+}
